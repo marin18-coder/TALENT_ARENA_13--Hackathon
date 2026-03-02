13682149b9105c730117b0af150285f6e851c177
marin18-coder <marinalapina18@gmail.com>
2026-03-02 11:11:02 +0100

    Add status device

--- api_device_reachability_status.py
+++ api_device_reachability_status.py
@@ -17,3 +17,32 @@ class DeviceStatus:
             "x-correlator": "b4333c46-49c0-4f62-80d7-f0ef930f1c46",
         }
 
+    def get_device_status(self) -> bool | None:
+        """
+            Fetch device status.
+
+            Returns:
+                bool: True if device is available, False if not, None if an error occurs.
+        """
+
+        payload = {"device": {"phoneNumber": self.phone_number}}
+
+        try:
+            response = requests.post(self.BASE_URL, json=payload, headers=self.headers, timeout=10)
+            data = response.json()
+            connectivity = data.get("connectivityStatus", "")
+            if connectivity in ["CONNECTED_SMS", "CONNECTED_DATA"]:
+                return True
+            elif connectivity in ["NOT_CONNECTED", ""]:
+                return False
+            return None
+
+        except requests.RequestException as exc:
+            print(f"Request failed: {exc}")
+            return None
+        except ValueError:
+            print("Invalid JSON response")
+            return None
+        
+        
+
